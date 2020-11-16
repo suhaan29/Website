c44dc6ca2bf69fc3005c9681845812b05c2c1cb6
suhaan29 <41914471+suhaan29@users.noreply.github.com>
2020-11-16 20:54:32 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function()
+{
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    $toggleCollapse.click(function()
+    {
+        $nav.toggleClass('collapse');
+    })
+})
+
